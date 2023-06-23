0b48f42cc5a80193445d3e06437406ceac140570
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2023-06-23 15:24:14 +1000

    Add fairyTale model

---
+++ Model/FairyTale.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FairyTalesApp.Model
+{
+    public class FairyTale
+    {
+        public string Name { get; set; }    
+        public TimeSpan ReadTime { get; set; }
+        public bool IsSave { get; set; }    
+        public string Image {get; set; }    
+    }
+}
